0ecbfc430deafcc5bc6184dfaaafef9fba5f471c
mshao <mshao@splunk.com>
2014-10-27 16:08:21 +0800

    add test code

---
+++ test/__init__.py
@@ -0,0 +1,15 @@
+import ssl
+
+import docker
+
+
+boot2docker = "192.168.59.103:2376"
+
+tls_config = docker.tls.TLSConfig(verify='/Users/cesc/.boot2docker/certs/boot2docker-vm/ca.pem',
+                                  client_cert=('/Users/cesc/.boot2docker/certs/boot2docker-vm/cert.pem',
+                                               '/Users/cesc/.boot2docker/certs/boot2docker-vm/key.pem'),
+                                  ssl_version=ssl.PROTOCOL_TLSv1,
+                                  assert_hostname=False)
+
+client = docker.Client(base_url='https://192.168.59.103:2376', tls=tls_config)
+print client.images()
