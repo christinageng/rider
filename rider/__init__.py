22ec06e0186adff1da4dffa3ccdf3d8ca0f0ce68
mshao <mshao@splunk.com>
2014-10-27 14:40:49 +0800

    add more files into the project

--- rider/__init__.py
+++ rider/__init__.py
@@ -8,3 +8,29 @@
 
 '''
 
+import sys
+from optparse import BadOptionError
+
+import os
+
+
+def create_main_parser():
+    pass
+
+
+def parse_opts(args):
+    return cmd_name, cmd_args
+
+
+def main():
+    """
+        main function
+    """
+
+    args = sys.argv[1:]
+    try:
+        cmd_name, cmd_args = parse_opts(args)
+    except BadOptionError, e:
+        sys.stderr.write(str(e))
+        sys.stderr.write(os.linesep)
+        sys.exit(1)
