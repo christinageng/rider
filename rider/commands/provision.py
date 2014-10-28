eb836d9080809c8e24323f9cf45818fce4469e32
mshao <mshao@splunk.com>
2014-10-28 11:57:38 +0800

    stable the container creation

--- rider/commands/provision.py
+++ rider/commands/provision.py
@@ -1,5 +1,6 @@
 import sys
 from optparse import Option, BadOptionError
+import time
 
 import os
 from rider.commands.base import Command
@@ -64,29 +65,34 @@ class ProvisionCommand(Command):
         master_name, container = scf.create_container(image=options.image_name, role=ROLE["MASTER"],
                                                       command="master")
 
-        # create license master node
-        license_path = os.path.dirname(options.license_file)
-        license_file_name = os.path.basename(options.license_file)
-        license_master_name, container = scf.create_container(image=options.image_name, role=ROLE["LICENSEMASTER"],
-                                                              command="lm",
-                                                              environment=[
-                                                                  'LICENSE_FILE=/license/' + license_file_name],
-                                                              binds={license_path:
-                                                                         {
-                                                                             'bind': '/license',
-                                                                             'ro': False}})
+        # create license master node only when specify the license file
+        if options.license_file:
+            license_path = os.path.dirname(options.license_file)
+            license_file_name = os.path.basename(options.license_file)
+            license_master_name, container = scf.create_container(image=options.image_name, role=ROLE["LICENSEMASTER"],
+                                                                  command="lm",
+                                                                  environment=[
+                                                                      'LICENSE_FILE=/license/' + license_file_name],
+                                                                  binds={license_path:
+                                                                             {
+                                                                                 'bind': '/license',
+                                                                                 'ro': False}})
+            time.sleep(3)  # some work round
+
+        # decide the links if the license_master is not existed
+        links = [(master_name, 'master')] if not options.license_file else links = [(license_master_name, 'lm'),
+                                                                                    (master_name, 'master')]
         # create indexer
         for i in range(0, int(options.indexer_num)):
             container_name, container = scf.create_container(image=options.image_name, role=ROLE["INDEXER"],
                                                              command="indexer",
-                                                             links=[(license_master_name, 'licensemaster'),
-                                                                    (master_name, 'master')]
+                                                             links=links
             )
+            time.sleep(2)  # some work round
 
         # create search head
         for i in range(0, int(options.sh_num)):
             container_name, container = scf.create_container(image=options.image_name, role=ROLE["SEARCHHEAD"],
                                                              command="sh",
-                                                             links=[(license_master_name, 'licensemaster'),
-                                                                    (master_name, 'master')]
-            )
+                                                             links=links)
+            time.sleep(2)  # some work round
